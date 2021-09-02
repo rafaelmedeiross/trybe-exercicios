1474bba33fb0d6cd9784d15edf6ba2f9e7781beb
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-02 18:15:38 -0300

    Exercicios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
@@ -32,3 +32,22 @@ const listaKeys = (objeto) => {
 }
 
 listaKeys(lesson3)
+
+const tamObj = (objeto) => {
+ const chavesObjeto = Object.keys(objeto);
+ const valoresObjeto = Object.values(objeto);
+ let cont1 = 0;
+ let cont2 = 0;
+
+ for (let k in chavesObjeto) {
+  cont1 += 1;
+ }
+ for (let k in valoresObjeto) {
+  cont2 += 1;
+ }
+
+ console.log(`O tamanho é de ${cont1} chaves e de ${cont2} valores totais`);
+
+}
+
+tamObj(lesson3)
